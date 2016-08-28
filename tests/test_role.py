391408c8eda250cf7f691cde0c762c2b75878d46
John Freeman <john.freeman.opensource@gmail.com>
2016-08-28 22:28:40 +0100

    Specify hosts to testinfa via python
    Was specifying hosts via molecule.yml file. The new approach is dynamic and avoids repeating configuration information.

--- tests/test_role.py
+++ tests/test_role.py
@@ -1,3 +1,8 @@
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+
+
 def test_dockbarx_config(Command):
     # Need to -set-home when using sudo for gconftool-2 to work
     output = Command.check_output("sudo --user=test_usr --set-home gconftool-2 --get /apps/dockbarx/launchers")
