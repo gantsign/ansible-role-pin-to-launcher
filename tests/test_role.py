476b16a9ac8a321a5b37e55b393b3c94558575f3
John Freeman <john.freeman.opensource@gmail.com>
2016-09-06 09:51:57 +0100

    Upgraded to Molecule 1.10.3
    Fixed flake8 linter warning.

--- tests/test_role.py
+++ tests/test_role.py
@@ -5,11 +5,17 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 def test_dockbarx_config(Command):
     # Need to -set-home when using sudo for gconftool-2 to work
-    output = Command.check_output("sudo --user=test_usr --set-home gconftool-2 --get /apps/dockbarx/launchers")
-    assert output == '[test-app;/usr/share/applications/test-app.desktop,test-id;/usr/share/applications/test-app2.desktop]'
+    output = Command.check_output("sudo %s --set-home gconftool-2 --get %s",
+                                  "--user=test_usr",
+                                  "/apps/dockbarx/launchers")
+    launchers = ['test-app;/usr/share/applications/test-app.desktop',
+                 'test-id;/usr/share/applications/test-app2.desktop']
+    assert output == '[' + ','.join(launchers) + ']'
+
 
 def test_unity_config(File):
-    config_file = File('/usr/share/glib-2.0/schemas/20_ansible_launcher.gschema.override')
+    path = '/usr/share/glib-2.0/schemas/20_ansible_launcher.gschema.override'
+    config_file = File(path)
 
     assert config_file.exists
     assert config_file.is_file
@@ -18,5 +24,8 @@ def test_unity_config(File):
     assert oct(config_file.mode) == '0644'
 
     # File.contains uses grep
-    expected_pattern = r"\['application://test-app3.desktop', 'application://test-app4.desktop', 'unity://running-apps'\]"
+    favorites = ['application://test-app3.desktop',
+                 'application://test-app4.desktop',
+                 'unity://running-apps']
+    expected_pattern = r"\['" + "', '".join(favorites) + r"'\]"
     assert config_file.contains(expected_pattern)
