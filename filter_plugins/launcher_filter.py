b86bbf23526561bc7cbaf5a018d1d21afe375ab7
John Freeman <john.freeman.opensource@gmail.com>
2018-08-21 19:32:40 +0100

    Added support for Gnome desktop (#63)
    The new default for Ubuntu.

--- filter_plugins/launcher_filter.py
+++ filter_plugins/launcher_filter.py
@@ -29,6 +29,28 @@ def to_dockbarx_items(context, pin_to_launcher_favorites):
 
 
 @contextfilter
+def to_gnome_items(context, pin_to_launcher_favorites):
+    '''
+    returns the Gnome launcher items
+    '''
+
+    pin_to_launcher_favorites = list(pin_to_launcher_favorites)
+
+    omit = context.resolve('omit')
+
+    launcher_items = []
+
+    for favourite in pin_to_launcher_favorites:
+        application = favourite.get('application')
+        if application not in ('', None, omit):
+            when_desktop = favourite.get('when_desktop')
+            if when_desktop in ('gnome', None):
+                launcher_items.append("'" + application + "'")
+
+    return launcher_items
+
+
+@contextfilter
 def to_unity_items(context, pin_to_launcher_favorites):
     '''
     returns the Unity launcher items
@@ -60,5 +82,6 @@ class FilterModule(object):
     def filters(self):
         return {
             'to_dockbarx_items': to_dockbarx_items,
+            'to_gnome_items': to_gnome_items,
             'to_unity_items': to_unity_items
         }
