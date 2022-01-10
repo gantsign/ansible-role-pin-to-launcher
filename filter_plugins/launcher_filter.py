82dccc0d33ca6bb9b54aa094a11eaba949b55ee3
John Freeman <john.freeman.opensource@gmail.com>
2022-01-10 21:26:54 +0000

    Dropped support for DockbarX (#90)
    I haven't used it in years and I don't have time to maintain support for features I don't use.

--- filter_plugins/launcher_filter.py
+++ filter_plugins/launcher_filter.py
@@ -1,35 +1,4 @@
-import re
-
 from jinja2.filters import contextfilter
-
-
-@contextfilter
-def to_dockbarx_items(context, pin_to_launcher_favorites):
-    '''
-    returns the DockbarX launcher items
-    '''
-
-    pin_to_launcher_favorites = list(pin_to_launcher_favorites)
-
-    omit = context.resolve('omit')
-
-    launcher_items = []
-
-    for favourite in pin_to_launcher_favorites:
-        application = favourite.get('application')
-        if application not in ('', None, omit):
-            when_desktop = favourite.get('when_desktop')
-            if when_desktop in ('dockbarx', None):
-                application_id = favourite.get('application_id')
-                if application_id is None:
-                    application_id = re.sub(
-                        '(.*)\\.desktop$', '\\1', application)
-                launcher_items.append(
-                    application_id +
-                    ';/usr/share/applications/' +
-                    application)
-
-    return launcher_items
 
 
 @contextfilter
@@ -59,6 +28,5 @@ class FilterModule(object):
 
     def filters(self):
         return {
-            'to_dockbarx_items': to_dockbarx_items,
             'to_gnome_items': to_gnome_items
         }
