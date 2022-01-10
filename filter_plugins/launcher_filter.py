bde0c928aa97cca28d455484acd2ad66500aaca6
John Freeman <john.freeman.opensource@gmail.com>
2022-01-10 21:04:18 +0000

    Dropped support for the Unity Desktop (#89)
    Canonical have ended standard support for Ubuntu Xenial (the last release using the Unity Desktop).

--- filter_plugins/launcher_filter.py
+++ filter_plugins/launcher_filter.py
@@ -54,38 +54,11 @@ def to_gnome_items(context, pin_to_launcher_favorites):
     return launcher_items
 
 
-@contextfilter
-def to_unity_items(context, pin_to_launcher_favorites):
-    '''
-    returns the Unity launcher items
-    '''
-
-    pin_to_launcher_favorites = list(pin_to_launcher_favorites)
-
-    omit = context.resolve('omit')
-
-    launcher_items = []
-
-    for favourite in pin_to_launcher_favorites:
-        application = favourite.get('application')
-        if application not in ('', None, omit):
-            when_desktop = favourite.get('when_desktop')
-            if when_desktop in ('unity', None):
-                launcher_items.append("'application://" + application + "'")
-
-        unity = favourite.get('unity')
-        if unity not in ('', None, omit):
-            launcher_items.append("'unity://" + unity + "'")
-
-    return launcher_items
-
-
 class FilterModule(object):
     ''' Launcher filter '''
 
     def filters(self):
         return {
             'to_dockbarx_items': to_dockbarx_items,
-            'to_gnome_items': to_gnome_items,
-            'to_unity_items': to_unity_items
+            'to_gnome_items': to_gnome_items
         }
--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -23,21 +23,3 @@ def test_gnome_config(host):
     favorites = ['test-app3.desktop', 'test-app6.desktop']
     expected_pattern = r"\[ '" + "', '".join(favorites) + r"' \]"
     assert config_file.contains(expected_pattern)
-
-
-def test_unity_config(host):
-    path = '/usr/share/glib-2.0/schemas/20_ansible_launcher.gschema.override'
-    config_file = host.file(path)
-
-    assert config_file.exists
-    assert config_file.is_file
-    assert config_file.user == 'root'
-    assert config_file.group == 'root'
-    assert config_file.mode == 0o644
-
-    # File.contains uses grep
-    favorites = ['application://test-app3.desktop',
-                 'application://test-app4.desktop',
-                 'unity://running-apps']
-    expected_pattern = r"\['" + "', '".join(favorites) + r"'\]"
-    assert config_file.contains(expected_pattern)
